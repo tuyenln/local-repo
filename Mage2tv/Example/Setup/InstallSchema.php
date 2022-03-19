f4870156db34cb73a8bee57dbb9d866aaded1007
TuyenLN <tuyenlnse@gmail,com>
2022-03-19 14:53:23 +0700

    init

---
+++ Mage2tv/Example/Setup/InstallSchema.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Mage2tv\Example\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        echo __METHOD__ . PHP_EOL;
+    }
+}
---
+++ Mage2tv/Example/Setup/UpgradeSchema.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mage2tv\Example\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        echo __METHOD__ . PHP_EOL;
+    }
+}
